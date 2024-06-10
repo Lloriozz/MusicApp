16fe75bcb1af1348c8e1a4b5e9dfcbd8ffc6179b
Dlong_ng <longnguyendinh120505@gmail.com>
2024-06-10 15:31:52 +0700

    push MusicApp to Github

---
+++ src/ImageSetting.java
@@ -0,0 +1,28 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+public class ImageSetting implements ImageAdjustment{
+        public static ImageIcon loadImage(String imagePath) {
+            try {
+                BufferedImage image = ImageIO.read(new File(imagePath));
+                return new ImageIcon(image);
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+        public static ImageIcon scaleImage(String path) {
+            try {
+                Image img = ImageIO.read(new File(path));
+                Image scaledImg = img.getScaledInstance(225, 225, Image.SCALE_SMOOTH);
+                return new ImageIcon(scaledImg);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+}
+
